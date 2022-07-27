d761fc96fdb0b47196772aac8705fdb0ad7192c2
Mihir Aswal <montyaswal0@gmail.com>
2022-07-27 17:58:53 +0530

    Change signup to auth

--- crown-clothings/src/App.js
+++ crown-clothings/src/App.js
@@ -1,7 +1,7 @@
  import { Routes,Route,Outlet } from "react-router-dom";
  import Home from "./components/routes/home/home.component";
  import Navigation from "./components/navigation/navigation.components";
-import SignIn from "./components/routes/sign-in/sign-in.components";
+import Authentication from "./components/routes/authentication/authentication.components";
 
  const Shop=()=>{
    return(
@@ -16,7 +16,7 @@ import SignIn from "./components/routes/sign-in/sign-in.components";
       <Route path="/" element={<Navigation/>}>
         <Route index={true} element={<Home/>}/>
         <Route path="shop" element={<Shop/>}/>
-        <Route path="sign-in" element={<SignIn/>}/>
+        <Route path="auth" element={<Authentication/>}/>
 
       </Route>
         
