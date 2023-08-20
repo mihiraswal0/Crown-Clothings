baa98a01f16f307bb8dd3191a143c7a69cc5fa65
Mihir Aswal <montyaswal0@gmail.com>
2023-08-20 19:54:34 +0530

    added slider

--- crown_clothing/src/pages/Home.jsx
+++ crown_clothing/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
-
+import Slider from '../components/Slider';
 const Home = () => {
   return (
     <div>
         <Navbar/>
+        <Slider/>
         </div>
   )
 }
