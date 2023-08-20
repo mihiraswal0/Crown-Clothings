0beefec02cc83d95e3428acacfef34ee8baf67c4
Mihir Aswal <montyaswal0@gmail.com>
2023-08-20 21:24:59 +0530

    categories for frontend added

--- crown_clothing/src/pages/Home.jsx
+++ crown_clothing/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import Slider from '../components/Slider';
+import Categories from '../components/Categories';
 const Home = () => {
   return (
     <div>
         <Navbar/>
         <Slider/>
+        <Categories/>  
         </div>
   )
 }
