dc6bd6d8a117074c2d8f7973615cd5a28238bf96
Mihir Aswal <montyaswal0@gmail.com>
2023-08-20 23:47:52 +0530

    added product card

--- crown_clothing/src/pages/Home.jsx
+++ crown_clothing/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Slider from '../components/Slider';
 import Categories from '../components/Categories';
+import Products from '../components/Products';
 const Home = () => {
   return (
     <div>
         <Navbar/>
         <Slider/>
-        <Categories/>  
+        <Categories/> 
+        <Products/> 
         </div>
   )
 }
