e90ae5b60dbc5b4d00b00f362286b978defe9182
Mihir Aswal <montyaswal0@gmail.com>
2023-08-20 16:04:34 +0530

    making again becaoz bugs

--- crown-clothings/src/components/utils/stripe/stripe.utils.js
+++
@@ -1,4 +0,0 @@
-import { loadStripe } from "@stripe/stripe-js";
-
-export const stripePromise = loadStripe(
-"pk_test_51LebC1SI0zwFEoXd6dWqGhx90AzFngcXyR6s94TUeOacIspTMjVp3Hu8qHk17mRoKFYdKGBGw8S2c0UEZi6LFA5Q00rieQ7M2g");
