9163abcab4f5ba1be66de8cdc967569ab7227e52
Mihir Aswal <montyaswal0@gmail.com>
2023-08-22 10:02:51 +0530

    cart made

--- crown_clothing/src/App.jsx
+++ crown_clothing/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Home from './pages/Home';
 import Product from './pages/Product';
+import Register from './pages/Register';
+import Cart from './pages/Cart';
 const App = () => {
   return (
-    <Product/>
+    <Cart/>
   )
 }
 
