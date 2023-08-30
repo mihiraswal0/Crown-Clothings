213a12f93d85fc82fd3cf376e1dad175c42a2693
Mihir Aswal <montyaswal0@gmail.com>
2023-08-30 08:02:24 +0530

    deleted by mistake

--- server/database/db.js
+++
@@ -1,8 +0,0 @@
- const mongoose = require('mongoose');
- require('dotenv').config();
-//  console.log(process.env.MONGO_URL)
- mongoose.connect(process.env.MONGO_URL)
-.then(()=>
- console.log("Connected to Database")).catch((err)=>{
-    console.log(err.message);
- });
