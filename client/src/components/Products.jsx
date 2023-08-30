1acd1d8f45971d9dd72b9807eaa2c8b5bae05f66
Mihir Aswal <montyaswal0@gmail.com>
2023-08-30 23:52:18 +0530

    bazzar cart added

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -19,7 +19,7 @@ const Products = ({ products }) => {
       {/* =================== Products Start here ================= */}
       <div className="max-w-screen-xl mx-auto grid grid-cols-4 gap-10 py-10">
       {products.map((item) => (
-          <ProductsCard key={item._id} product={item} />
+          <ProductsCard key={item.id} product={item} />
         ))}
       </div>
       {/* =================== Products End here =================== */}
--- client/src/components/ProductsCard.jsx
+++ client/src/components/ProductsCard.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { BsArrowRight } from 'react-icons/bs'
+import { useDispatch } from 'react-redux';
 import { Navigate, useNavigate } from 'react-router-dom';
+import { addToCart } from '../redux/bazarSlice';
 const ProductsCard = ({product}) => {
-  const navigate=useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  // console.log(product.id);
   const _id = product.title;
   const idString = (_id) => {
     return String(_id).toLowerCase().split(" ").join("");
@@ -17,64 +21,75 @@ const ProductsCard = ({product}) => {
   };
   return (
     <div className="w-full relative group">
-    <div
-      onClick={handleDetails}
-      className="w-full h-96 cursor-pointer overflow-hidden"
-    >
-      <img
-        className="w-full h-full object-cover group-hover:scale-110 duration-500"
-        src={product.image}
-        alt="productImg"
-      />
-    </div>
-    <div className="w-full border-[1px] px-2 py-4">
-      <div className="flex justify-between items-center">
-        <div>
-          <h2 className="font-titleFont text-base font-bold">
-            {product.title.substring(0, 15)}
-          </h2>
-        </div>
-        <div className="text-sm relative w-28 flex justify-end overflow-hidden">
-          <div className="flex gap-2 transform group-hover:translate-x-24 transition-transform duration-500">
-            <p className="line-through text-gray-500">${product.oldPrice}</p>
-            <p className="font-semibold">${product.price}</p>
+      <div
+        onClick={handleDetails}
+        className="w-full h-96 cursor-pointer overflow-hidden"
+      >
+        <img
+          className="w-full h-full object-cover group-hover:scale-110 duration-500"
+          src={product.image}
+          alt="productImg"
+        />
+      </div>
+      <div className="w-full border-[1px] px-2 py-4">
+        <div className="flex justify-between items-center">
+          <div>
+            <h2 className="font-titleFont text-base font-bold">
+              {product.title.substring(0, 15)}
+            </h2>
           </div>
-          <p
-            
-            className="absolute z-20 w-[100px] text-gray-500 hover:text-gray-900 flex items-center gap-1 top-0 transform -translate-x-32 group-hover:translate-x-0 transition-transform cursor-pointer duration-500"
-          >
-            Add Cart
-            <span>
-              <BsArrowRight />
-            </span>
-          </p>
+          <div className="text-sm relative w-28 flex justify-end overflow-hidden">
+            <div className="flex gap-2 transform group-hover:translate-x-24 transition-transform duration-500">
+              <p className="line-through text-gray-500">${product.oldPrice}</p>
+              <p className="font-semibold">${product.price}</p>
+            </div>
+            <p
+              onClick={() =>
+                dispatch(
+                  addToCart({
+                    _id: product.id,
+                    title: product.title,
+                    image: product.image,
+                    price: product.price,
+                    quantity: 1,
+                    description: product.description,
+                  })
+                )
+              }
+              className="absolute z-20 w-[100px] text-gray-500 hover:text-gray-900 flex items-center gap-1 top-0 transform -translate-x-32 group-hover:translate-x-0 transition-transform cursor-pointer duration-500"
+            >
+              add to cart
+              <span>
+                <BsArrowRight />
+              </span>
+            </p>
+          </div>
+        </div>
+        <div>
+          <p>{product.category}</p>
         </div>
       </div>
-      <div>
-        <p>{product.category}</p>
+      <div className="absolute top-4 right-0">
+        {product.isNew && (
+          <p className="bg-black text-white font-semibold font-titleFont px-6 py-1">
+            Sale
+          </p>
+        )}
       </div>
+      {/* <ToastContainer
+        position="top-left"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      /> */}
     </div>
-    <div className="absolute top-4 right-0">
-      {product.isNew && (
-        <p className="bg-black text-white font-semibold font-titleFont px-6 py-1">
-          Sale
-        </p>
-      )}
-    </div>
-    {/* <ToastContainer
-      position="top-left"
-      autoClose={3000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="dark"
-    /> */}
-  </div>
-  )
-}
+  );
+};
 
 export default ProductsCard
