9af59607850e8cf66263595db041977aa02a3f75
Mihir Aswal <montyaswal0@gmail.com>
2023-08-30 16:27:21 +0530

    routing added

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,52 @@
 import Banner from "./components/Banner";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import { Outlet,RouterProvider,ScrollRestoration,createBrowserRouter} from "react-router-dom";
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <ScrollRestoration />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        // loader: productsData,
+      },
+      // {
+      //   path: "/product/:id",
+      //   element: <Product />,
+      // },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+]);
+
 export default function App() {
   return (
-   <div className="font-bodyFont">
-    <Header/>
-    <Home/>
-   </div>
+    <div className="font-bodyFont">
+    <RouterProvider router={router} />
+  </div>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Banner from '../components/Banner'
 import Products from '../components/Products'
-import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
         <Banner/>
         <Products/>
-        <Footer/>
+        
     </div>
   )
 }
